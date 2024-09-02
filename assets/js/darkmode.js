f52074b04f16376b80fc53d20c070644b75e501e
mani <mani.84.zmn@gmail.com>
2024-09-02 17:08:42 +0330

    add dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,6 +1,6 @@
 const btnDark = document.getElementById("dark-mode");
 
-btnDark-addEventListener("click", (e) => {
+btnDark.addEventListener("click", (e) => {
     e.preventDefault();
     document.documentElement.classList.toggle("dark");
-})
+});
