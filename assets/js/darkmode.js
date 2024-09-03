15ef285a537f0262bdf908907e96038433d0c23e
mani <mani.84.zmn@gmail.com>
2024-09-03 13:01:43 +0330

    finish sidebar for desktop and mobile with dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -11,3 +11,33 @@ btnLight.addEventListener("click", (e) => {
     e.preventDefault();
     document.documentElement.classList.toggle("dark");
 });
+
+const sidebar = document.getElementById("sidebar");
+
+const closeSidebarLight = document.getElementById("close-light");
+
+closeSidebarLight.addEventListener("click", (e) => {
+    e.preventDefault();
+    sidebar.classList.toggle("hidden");
+})
+
+const closeSidebardark = document.getElementById("close-dark");
+
+closeSidebardark.addEventListener("click", (e) => {
+    e.preventDefault();
+    sidebar.classList.toggle("hidden");
+})
+
+const humbergurLight = document.getElementById("humbergermenu-light")
+
+humbergurLight.addEventListener("click", (e) => {
+    e.preventDefault();
+    sidebar.classList.toggle("hidden");
+})
+
+const humbergurdark = document.getElementById("humbergermenu-dark")
+
+humbergurdark.addEventListener("click", (e) => {
+    e.preventDefault();
+    sidebar.classList.toggle("hidden");
+})
